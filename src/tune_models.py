04c4793525b29116be4966f6f11abb260b68bef3
HrisheetaRoy <hrisheetaroy@gmail.com>
2025-07-05 08:58:36 +0530

    tuned the model

---
+++ src/tune_models.py
@@ -0,0 +1,13 @@
+from sklearn.model_selection import GridSearchCV
+
+def tune_random_forest(X, y):
+    from sklearn.ensemble import RandomForestRegressor
+    rf = RandomForestRegressor(random_state=42)
+    param_grid = {
+        'n_estimators': [100, 200],
+        'max_depth': [None, 10, 20],
+        'min_samples_split': [2, 5]
+    }
+    grid = GridSearchCV(rf, param_grid, cv=3, scoring='r2', n_jobs=-1)
+    grid.fit(X, y)
+    return grid.best_estimator_, grid.best_params_
